dbbe04847219cd915437797f04f2faf0d9172880
salmonco <xkqjsslsek80@naver.com>
2024-06-29 18:28:38 +0900

    Test: local server

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -3,6 +3,7 @@ import Config from 'react-native-config';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: Config.API_URL,
+  // baseURL: 'http://172.30.1.72:8080',
 });
 
 export default axiosInstance;
